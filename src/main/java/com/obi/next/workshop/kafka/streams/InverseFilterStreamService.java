6bad9f6d36f04046b364b810aa6d7cc3bb17054e
Arkadiusz Czarnik <arkadiusz.czarnik@obi.de>
2018-01-03 22:32:01 +0100

    add examples.

---
+++ src/main/java/com/obi/next/workshop/kafka/streams/InverseFilterStreamService.java
@@ -0,0 +1,36 @@
+package com.obi.next.workshop.kafka.streams;
+
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.kstream.KStream;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+@Service
+public class InverseFilterStreamService {
+
+    private KafkaConfiguration kafkaConfiguration = new KafkaConfiguration();
+    private KafkaStreams kafkaStreams = buildStream();
+
+    private KafkaStreams buildStream() {
+        StreamsBuilder builder = new StreamsBuilder();
+        KStream<String, String> stream = builder.stream("weather");
+        stream.filterNot((key, value) -> value.length() > 1)
+                .to("inverse_length_out");
+        return new KafkaStreams(builder.build(), kafkaConfiguration.configuration());
+    }
+
+    @PostConstruct
+    private void init() {
+        kafkaStreams.start();
+    }
+
+    @PreDestroy
+    private void destroy() {
+        kafkaStreams.close();
+    }
+
+
+}
--- src/main/java/com/obi/next/workshop/kafka/streams/SimpleStreamService.java
+++ src/main/java/com/obi/next/workshop/kafka/streams/SimpleStreamService.java
@@ -3,13 +3,14 @@ package com.obi.next.workshop.kafka.streams;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Printed;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
 @Service
-public class StreamService {
+public class SimpleStreamService {
 
     private KafkaConfiguration kafkaConfiguration = new KafkaConfiguration();
     private KafkaStreams kafkaStreams = buildStream();
@@ -17,13 +18,9 @@ public class StreamService {
     private KafkaStreams buildStream() {
         StreamsBuilder builder = new StreamsBuilder();
         KStream<String, String> stream = builder.stream("weather");
-        stream.filter((key, value) -> {
-            return true;
-        }).to("frost");
+        stream.print(Printed.toSysOut());
         return new KafkaStreams(builder.build(), kafkaConfiguration.configuration());
     }
-
-
 
     @PostConstruct
     private void init() {
